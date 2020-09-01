b8d99f5953a6a0e8a99f1423b5d7bd3dd0eaa55a
roerohan <roerohan@gmail.com>
2020-09-01 20:36:35 +0000

    Deploying to gh-pages from  @ 1b5867a4083c84aa3d4a0c1049907cca09f11862 🚀

---
+++ precache-manifest.4b4bf222b4105c1041792fbfa26de4d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941ea6a458d913021721d96ab92b0b66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c404bd7ee7b9da1abd1d",
+    "url": "/static/css/main.7d7deb05.chunk.css"
+  },
+  {
+    "revision": "f4612ee428284e80cbfe",
+    "url": "/static/js/2.2a37aacc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a37aacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c404bd7ee7b9da1abd1d",
+    "url": "/static/js/main.3ba99bda.chunk.js"
+  },
+  {
+    "revision": "0ef4956232956e760218",
+    "url": "/static/js/runtime-main.71634857.js"
+  }
+]);
